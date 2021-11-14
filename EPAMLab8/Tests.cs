547a387c5e74311b22fb0916985735bb4c13ce22
Makar Dezhemesov <Makar.Dzezhamesau@itechart-group.com>
2021-11-14 16:25:39 +0300

    Selenium init

---
+++ EPAMLab8/Tests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Chrome;
+
+namespace EpamLab8
+{
+    [TestFixture]
+    public class Test
+    {
+        public static IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--ignore-certificate-errors");
+            options.AddArguments("--ignore-ssl-errors");
+
+            driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+        }
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
+        {
+            // ТУТ КОД
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+            // ТУТ КОД
+        }
+
+        [Test]
+        public void TEST_1()
+        {
+            // ТУТ КОД
+        }
+
+        [Test]
+        public void TEST_2()
+        {
+            // ТУТ КОД
+        }
+    }
+}
